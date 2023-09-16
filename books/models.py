e502ebbbbe1c6ee91075c54004a2d2e7021a911b
jonascarvalh <olaffic@gmail.com>
2023-09-16 12:24:29 -0300

    fix id_book with non editable

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 # Create your models here.
 class Books(models.Model):
-    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=True)
+    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     release_year = models.IntegerField()
