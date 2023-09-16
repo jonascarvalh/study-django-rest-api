fed3c66b0db428feeb44032d0b8c79a876028d59
jonascarvalh <olaffic@gmail.com>
2023-09-16 13:55:47 -0300

    config authentication rule for books api in your viewsets

--- books/api/viewsets.py
+++ books/api/viewsets.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from books.api import serializers
 from books import models
 
 class BooksViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
+
     serializer_class = serializers.BooksSerializer
     queryset = models.Books.objects.all()
